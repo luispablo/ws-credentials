ceba1d9a9c3f9fe4732b64a68147cc2f178e8bf3
Unknown <luispablo.gallo@gmail.com>
2018-04-24 15:27:55 -0300

    Replace tabs with spaces (2)

--- test/WSCredentials-test.js
+++ test/WSCredentials-test.js
@@ -12,83 +12,83 @@ var server;
 var USERNAME = "tsmith";
 var PASSWORD = "password";
 var mockDetails = {
-	CN: "Tom Smith",
-	Description: null,
-	DisplayName: "Tom Wilson Smith",
-	GivenName: "Tom",
-	GroupType: null,
-	HomeDirectory: "\\SVRSOME\HOME\tsmith",
-	Mail: "tsmith@svrhome.com",
-	MailNickname: USERNAME,
-	MemberOf: ["PaperCut", "OtherGroup"],
-	Name: "Tom Wilson Smith",
-	SAMAccountName: USERNAME,
-	Surname: "Smith",
-	WhenChanged: "6/5/2016 12:00:26 PM",
-	WhenCreated: "8/31/2010 5:54:10 PM"
+  CN: "Tom Smith",
+  Description: null,
+  DisplayName: "Tom Wilson Smith",
+  GivenName: "Tom",
+  GroupType: null,
+  HomeDirectory: "\\SVRSOME\HOME\tsmith",
+  Mail: "tsmith@svrhome.com",
+  MailNickname: USERNAME,
+  MemberOf: ["PaperCut", "OtherGroup"],
+  Name: "Tom Wilson Smith",
+  SAMAccountName: USERNAME,
+  Surname: "Smith",
+  WhenChanged: "6/5/2016 12:00:26 PM",
+  WhenCreated: "8/31/2010 5:54:10 PM"
 };
 
 var mockResponse = { details: JSON.stringify(mockDetails), authenticate: "true", groups: mockDetails.MemberOf };
 
 app.use(function *() {
-	if (this.path === "/entry/"+ USERNAME +"/json") this.body = mockResponse.details;
-	else if (this.path === "/json/"+ USERNAME +"/"+ PASSWORD) this.body = mockResponse.authenticate;
-	else if (this.path === "/entry/"+ USERNAME +"/json") this.body = mockResponse.groups;
+  if (this.path === "/entry/"+ USERNAME +"/json") this.body = mockResponse.details;
+  else if (this.path === "/json/"+ USERNAME +"/"+ PASSWORD) this.body = mockResponse.authenticate;
+  else if (this.path === "/entry/"+ USERNAME +"/json") this.body = mockResponse.groups;
 });
 
 test("WSCredentials - [BEFORE] Start Koa server", assert => {
-	server = app.listen(TESTING_PORT);
-	assert.end();
+  server = app.listen(TESTING_PORT);
+  assert.end();
 });
 
 test("WSCredentials - details", function (assert) {
-	assert.plan(1);
-	credentials.details(USERNAME).then(function (userDetails) {
-		assert.deepEqual(userDetails, mockDetails, "The expected user details");
-	}).catch(function (err) {
-		assert.fail(err.message);
-	});
+  assert.plan(1);
+  credentials.details(USERNAME).then(function (userDetails) {
+    assert.deepEqual(userDetails, mockDetails, "The expected user details");
+  }).catch(function (err) {
+    assert.fail(err.message);
+  });
 });
 
 test("WSCredentials - authenticate OK", function (assert) {
-	assert.plan(1);
-	credentials.authenticate(USERNAME, PASSWORD).then(function (userDetails) {
-		assert.deepEqual(userDetails, mockDetails, "The expected user details");
-	}).catch(function (err) {
-		assert.fail(JSON.stringify(err));
-	});
+  assert.plan(1);
+  credentials.authenticate(USERNAME, PASSWORD).then(function (userDetails) {
+    assert.deepEqual(userDetails, mockDetails, "The expected user details");
+  }).catch(function (err) {
+    assert.fail(JSON.stringify(err));
+  });
 });
 
 test("WSCredentials - authenticate inexistent username", assert => {
-	assert.plan(1);
-	mockResponse.authenticate = "false";
-	credentials.authenticate(USERNAME, PASSWORD).then(function (res) {
-		assert.fail("Should not get a response");
-	}).catch(function (err) {
-		assert.equal(err.message, "Wrong username or password");
-	});
+  assert.plan(1);
+  mockResponse.authenticate = "false";
+  credentials.authenticate(USERNAME, PASSWORD).then(function (res) {
+    assert.fail("Should not get a response");
+  }).catch(function (err) {
+    assert.equal(err.message, "Wrong username or password");
+  });
 });
 
 test("WSCredentials - authenticate wrong password", assert => {
-	assert.plan(1);
-	mockResponse.authenticate = "false";
-	credentials.authenticate(USERNAME, PASSWORD).then(function (res) {
-		assert.fail("Should not get a response");
-	}).catch(function (err) {
-		assert.equal(err.message, "Wrong username or password");
-	});
+  assert.plan(1);
+  mockResponse.authenticate = "false";
+  credentials.authenticate(USERNAME, PASSWORD).then(function (res) {
+    assert.fail("Should not get a response");
+  }).catch(function (err) {
+    assert.equal(err.message, "Wrong username or password");
+  });
 });
 
 test("WSCredentials - groups", function (assert) {
-	assert.plan(1);
-		credentials.groups(USERNAME).then(function (groups) {
-		assert.deepEqual(groups, mockDetails.MemberOf, "The groups the user is member of");
-	}).catch(function (err) {
-		assert.fail(err.message);
-	});
+  assert.plan(1);
+    credentials.groups(USERNAME).then(function (groups) {
+    assert.deepEqual(groups, mockDetails.MemberOf, "The groups the user is member of");
+  }).catch(function (err) {
+    assert.fail(err.message);
+  });
 });
 
 test("WSCredentials - [AFTER] Close Koa server", assert => {
-	server.close();
-	assert.end();
+  server.close();
+  assert.end();
 });
